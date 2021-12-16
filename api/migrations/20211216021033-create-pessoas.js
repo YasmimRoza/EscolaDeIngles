5e00563f4eecdda9f4ca37645647c601d142f7c6
Yasmim Roza <yasmimroza16@gmail.com>
2021-12-15 23:40:44 -0400

    Make migration and model from table'Pessoas', register on database.

---
+++ api/migrations/20211216021033-create-pessoas.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Pessoas', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.BIGINT
+      },
+      nome: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      ativo: {
+        allowNull: false,
+        type: Sequelize.BOOLEAN
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      role: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Pessoas');
+  }
+};
