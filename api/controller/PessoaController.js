6946b83f145b784523c4004f007f24ad0b80a11b
Yasmim Roza <yasmimroza16@gmail.com>
2021-12-19 04:53:29 -0400

    Crud completo

--- api/controller/PessoaController.js
+++ api/controller/PessoaController.js
@@ -1,7 +1,7 @@
 const database = require('../models')
 
 class PessoaController{
-     async index(req, res){
+     async show(req, res){
         try{
             const todasAsPessoas = await database.Pessoas.findAll()
             return res.status(200).json(todasAsPessoas);
@@ -9,6 +9,79 @@ class PessoaController{
             return res.status(500).json(error.message);
         }
     }
+
+    async search(req, res){
+        const { id } = req.params
+
+        try{
+            const umaPessoa = await database.Pessoas.findOne({
+                where: {
+                    id: Number(id)
+                }
+            })
+
+            return res.status(200).json(umaPessoa)
+        }catch(error){
+            return res.status(500).json(error.message)
+        }
+    }
+
+    //Cria uma nova pessoa
+    async create(req, res){
+        const novaPessoa = req.body;
+
+        try{
+            const novaPessoaCriada = await database.Pessoas.create(novaPessoa)
+            
+            return res.status(200).json(novaPessoaCriada)
+        }catch(error){
+            return res.status(500).json(error.message)
+        }
+
+    }
+
+    //Altera uma pessoa
+    async update(req, res){
+        const { id } = req.params
+        const novasInfos = req.body
+
+        try{
+            await database.Pessoas.update(novasInfos, 
+                {
+                    where: {
+                        id: Number(id)
+                    }
+                })
+            
+            const pessoaAtualizada = await database.Pessoas.findOne({
+                where: {
+                    id: Number(id)
+                }
+            })
+
+            return res.status(200).json(pessoaAtualizada)
+        }catch(error){
+            return res.status(500).json(error.message)
+        }
+    }
+
+    async delete(req, res){
+        const { id } = req.params
+        try{
+            await database.Pessoas.destroy({
+                where: {
+                    id: Number(id)
+                }
+            })
+
+            return res.status(200).json({
+                mensagem: `id ${id} deletado`
+            })
+        }catch(error){
+            return res.status(500).json(error.message)
+        }
+    }
+
 }
 
 module.exports = new PessoaController;
