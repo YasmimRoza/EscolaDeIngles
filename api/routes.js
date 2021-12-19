883cf7e4820e19c8e8821c36d32819975633c9d0
Yasmim Roza <yasmimroza16@gmail.com>
2021-12-19 04:56:57 -0400

    Feito as rotas do crud de pessoas.

--- api/routes.js
+++ api/routes.js
@@ -2,6 +2,11 @@ const { Router } = require('express');
 const router = Router();
 const PessoaController = require('./controller/PessoaController')
 
-router.get('/', PessoaController.index)
+router.get('/listapessoas', PessoaController.show)
+router.get('/pessoas/:id', PessoaController.search)
+router.post('/pessoas', PessoaController.create)
+router.put('/pessoas/:id', PessoaController.update)
+router.delete('/pessoas/:id', PessoaController.delete)
+
 
 module.exports = router 
